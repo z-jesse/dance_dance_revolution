e626eb0597ce77f5fd645569493c875a2c6a0708
Jesse <43320266+z-jesse@users.noreply.github.com>
2023-05-06 22:30:29 -0500

    blink led

--- src/main.c
+++ src/main.c
@@ -1,1 +1,27 @@
-void app_main() {}
+#include "driver/gpio.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+
+
+#define LED_PIN 2
+
+TaskHandle_t blink_handle = NULL;
+
+void blink_led() {
+    static uint8_t led = 0;
+    gpio_set_direction(LED_PIN, GPIO_MODE_OUTPUT);
+    while (1)
+    {
+        gpio_set_level(LED_PIN, led);
+        led ^= 0x1;
+        vTaskDelay(10);
+    }   
+}
+
+void app_main() {
+    xTaskCreate(blink_led, "blink led", 4096, NULL, 10, &blink_handle);
+    while (1)
+    {
+        taskYIELD();
+    }   
+}
